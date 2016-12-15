06127a74ae91f3854e2aec1eebcd7b24d2cebef2
loveKino <842914439@qq.com>
2016-12-15 17:16:28 +0800

    add tests

--- benchmark/time.js
+++ benchmark/time.js
@@ -18,12 +18,27 @@ let test = (left, right) => {
 
     let t2 = new Date().getTime();
 
-    log(`time spending is ${t2 - t1}`);
+    log(`time spending is ${t2 - t1}ms`);
 };
 
 test('The default options copied by suite instances.', '(Object): The benchmark instance.');
 
-Promise.all([readFile(__dirname + '/fixture/test1/left.txt', 'utf-8'), readFile(__dirname + '/fixture/test1/right.txt', 'utf-8')]).then(([left, right]) => {
+Promise.all([
+    readFile(__dirname + '/fixture/test2/left.txt', 'utf-8'),
+    readFile(__dirname + '/fixture/test2/right.txt', 'utf-8')
+]).then(([left, right]) => {
+    try {
+        // TODO fix
+        test(left, right);
+    } catch (err) {
+        log(err);
+    }
+});
+
+Promise.all([
+    readFile(__dirname + '/fixture/test3/left.txt', 'utf-8'),
+    readFile(__dirname + '/fixture/test3/right.txt', 'utf-8')
+]).then(([left, right]) => {
     try {
         // TODO fix
         test(left, right);
